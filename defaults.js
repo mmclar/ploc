fd426efcebcd5cdee4f085c5752b0bcab736da0e
Michael McLarnon <mmclar@gmail.com>
2020-11-27 14:12:43 -0500

    small adjustment to defaults

--- defaults.js
+++ defaults.js
@@ -1,9 +1,9 @@
 $(() => {
     // For picture at https://www.robertharding.com/preview/1267-26/yarra-river-princes-bridge-melbourne-city-skyline-melbourne/
     ploc.setPoints([
-        ['af', [144.96821, -37.81566, 90.76003]],
+        ['af', [144.96821, -37.81565, 89.33548]],
         ['an', [144.96795, -37.81693, 67.09444]],
-        ['bf', [144.97084, -37.81499, 85.07790]],
+        ['bf', [144.97080, -37.81500, 82.45074]],
         ['bn', [144.96973, -37.81648, 63.05607]],
     ]);
     ploc.zoomToPoints();
